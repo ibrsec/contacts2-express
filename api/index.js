641817b8be9c9af1cdfad4707e0ad902ca2f9280
ibr <ibr.seckin@gmail.com>
2024-07-07 23:07:27 +0300

    vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 require('dotenv').config();
 const PORT = process.env.PORT || 6000;
-const errorHandler = require('./middleware/errorHandler.js');
-const connectDB = require('./config/dbConnection.js');
-const validateToken = require('./middleware/validateTokenHandler.js');
+const errorHandler = require('../middleware/errorHandler.js');
+const connectDB = require('../config/dbConnection.js');
+const validateToken = require('../middleware/validateTokenHandler.js');
 connectDB();
 
 const app = express();
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.get("/",(req,res)=>{res.send("server is running on "+ PORT)})
 
-app.use('/api/contacts',validateToken,require('./router/contactRouter.js'))
-app.use('/api/users',require('./router/userRouter.js'))
+app.use('/api/contacts',validateToken,require('../router/contactRouter.js'))
+app.use('/api/users',require('../router/userRouter.js'))
 
 app.use(errorHandler);
 app.listen(PORT,()=>console.log(`Server is running on: localhost:${PORT}`))
